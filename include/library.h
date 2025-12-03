77614d0355d9749b66e5c7ca8a777f28e3a4b095
nisthakachhwaha15 <nisthakachhwaha15@gmail.com>
2025-12-04 01:52:07 +0530

    Update library.h

--- include/library.h
+++ include/library.h
@@ -4,6 +4,7 @@
 #define MAX_BOOKS 100
 #define TITLE_LEN 50
 #define AUTHOR_LEN 50
+#define NAME_LEN 50
 
 // Book structure
 struct Book {
@@ -12,17 +13,26 @@ struct Book {
     char author[AUTHOR_LEN];
     int total_copies;
     int available_copies;
+    char last_borrower[NAME_LEN];  // NEW: who last borrowed this book
 };
 
-// Global variables
+// Global variables (defined in operations.c)
 extern struct Book library[MAX_BOOKS];
 extern int book_count;
 
-// Function prototypes
+// Core functions
+void loadBooks(const char *filename);
+void saveBooks(const char *filename);
+
 void addBook();
 void listBooks();
-void searchBook();
+void listAvailableBooks();
+void searchBookById();
+void searchBookByTitle();
+void searchBookByAuthor();   // NEW
 void issueBook();
 void returnBook();
+void deleteBook();
+void showStats();            // NEW
 
 #endif
